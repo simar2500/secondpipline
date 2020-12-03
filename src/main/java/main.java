88a338131a6a72156072ec4150966e7b38e7aaea
simar2500 <simar2500@gmail.com>
2020-12-02 21:37:44 -0500

    doing again from ec2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,5 +7,7 @@ public class main {
         System.out.println("now i have created a second pipline for this project ");
 
         System.out.println("this is the new ec2 pipeline which i wanted to check ");
+        System.out.println("trying again");
+
     }
 }
